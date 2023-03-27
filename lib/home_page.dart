f6fdc7751d48d7bb28c632d92c1991621d9f59dc
winisoft9356 <winisoft9356@gmail.com>
2023-03-27 13:11:26 +0530

    bottom nav temp

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,12 +9,37 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final iconList = <IconData>[
+    Icons.brightness_5,
+    Icons.brightness_4,
+    Icons.brightness_6,
+    Icons.brightness_7,
+  ];
+
   @override
   Widget build(BuildContext context) {
+    int _activeIndex = 1;
     return Scaffold(
       appBar: AppBar(
         title: const Text("Forex News App"),
         centerTitle: true,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      body: Container(
+        child: Center(child: Text("a $_activeIndex")),
+      ),
+      bottomNavigationBar: AnimatedBottomNavigationBar(
+        icons: iconList,
+        activeIndex: _activeIndex,
+        gapLocation: GapLocation.center,
+        notchSmoothness: NotchSmoothness.smoothEdge,
+        leftCornerRadius: 32,
+        rightCornerRadius: 32,
+        onTap: (i) {
+          setState(() {
+            _activeIndex = i;
+          });
+        },
       ),
     );
   }
